660ac1963c12d7043e4dfdc4267f9953878f5719
Piotress5 <piotrciesla222@gmail.com>
2023-11-03 03:14:59 +0100

    Add second animation

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const play = document.querySelectorAll("button.play");
 const ikony = document.querySelectorAll("button.play img");
 const stop = document.querySelectorAll("button.stop");
 const obiekty1 = document.querySelectorAll("div.obiekty1 div");
+const obiekty2 = document.querySelectorAll("div.obiekty2 div");
 let tempspace = "";
 let tempvalue = 0;
 let wartosc = "";
@@ -17,18 +18,34 @@ play.forEach(function(x) {
             if (wartosc == 1) {
                 ikony[0].src = "img/pause-button.png";
                 for (var i = 0, max = obiekty1.length; i < max; i++) {
-                    obiekty1[i].style.animationName = "anim1_1_" + (i + 1);
+                    obiekty1[i].style.animationName = "anim" + wartosc + "_1_" + (i + 1);
                     obiekty1[i].style.animationPlayState = "running";
                 }
             }
         } else {
             if (wartosc == 1) {
-                ikony[0].src = "img/play-button.png";
+                ikony[tempvalue].src = "img/play-button.png";
                 for (var i = 0, max = obiekty1.length; i < max; i++) {
                     obiekty1[i].style.animationPlayState = "paused";
                 }
             }
         } // kopiowac do kazdego przycisku tego if'a
+        if (tempspace.includes("img/play-button.png") == true) {
+            if (wartosc == 2) {
+                ikony[tempvalue].src = "img/pause-button.png";
+                for (var i = 0, max = obiekty2.length; i < max; i++) {
+                    obiekty2[i].style.animationName = "anim" + wartosc + "_1_" + (i + 1);
+                    obiekty2[i].style.animationPlayState = "running";
+                }
+            }
+        } else {
+            if (wartosc == 2) {
+                ikony[tempvalue].src = "img/play-button.png";
+                for (var i = 0, max = obiekty2.length; i < max; i++) {
+                    obiekty2[i].style.animationPlayState = "paused";
+                }
+            }
+        }
     }
 });
 
@@ -36,12 +53,19 @@ stop.forEach(function(y) {
     y.addEventListener("click", zatrzymaj);
     function zatrzymaj() {
         wartosc = y.getAttribute("value");
+        tempvalue = wartosc - 1;
         if (wartosc == 1) {
             for (var i = 0, max = obiekty1.length; i < max; i++) {
                 obiekty1[i].style.animationName = "none";
             }
             ikony[tempvalue].src = "img/play-button.png";
         } // kopiowac do kazdego przycisku tego if'a
+        if (wartosc == 2) {
+            for (var i = 0, max = obiekty2.length; i < max; i++) {
+                obiekty2[i].style.animationName = "none";
+            }
+            ikony[tempvalue].src = "img/play-button.png";
+        }
     }
 });
 
